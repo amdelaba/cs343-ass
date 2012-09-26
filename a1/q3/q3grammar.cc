0cba6a30354c42015192c05120541a99669bd2aa
andres <adelabarra24@gmail.com>
2012-09-26 00:44:24 -0400

    Halfway done Q3

---
+++ a1/q3/q3grammar.cc
@@ -0,0 +1,128 @@
+// Command syntax:
+//   $ ./a.out  [ input-file ] 
+// all parameters are optional. Defaults: input=>cin
+//
+// Examples:
+//   $ ./a.out 
+//   $ ./a.out in
+
+#include <uC++.h>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+using namespace std;					// direct access to std
+#include <cstdlib>					// exit
+#include "q3grammar.h"
+#include <string>
+
+bool convert( int &val, char *buffer ) {		// convert C string to integer
+    std::stringstream ss( buffer );			// connect stream and buffer
+    ss >> dec >> val;					// convert integer from buffer
+    return ! ss.fail() &&				// conversion successful ?
+	// characters after conversion all blank ?
+	string( buffer ).find_first_not_of( " ", ss.tellg() ) == string::npos;
+} // convert
+
+
+
+void usage( char *argv[] ) {
+    cerr << "Usage: " << argv[0]
+	 << ") [ infile ]" << endl;
+    exit( EXIT_FAILURE );				// TERMINATE
+} // usage
+
+
+
+
+void uMain::main() {
+    istream *infile = &cin;				// default value
+    ostream *outfile = &cout;				// default value
+
+    switch ( argc ) {
+     case 2:
+	try {
+	    infile = new ifstream( argv[1] );
+	} catch( uFile::Failure ) {
+	    cerr << "Error! Could not open input file \"" << argv[1] << "\"" << endl;
+	    usage( argv );
+	} // try
+        // FALL THROUGH
+      case 1:						// all defaults
+        break;
+      default:						// wrong number of options
+	usage( argv );
+    } // switch
+
+    *infile >> noskipws;				// turn off white space skipping during input
+
+
+    /* char by char
+    char ch;
+ A: for ( ;; ) {  // for every line		   
+      Grammar grammar;
+    B: for ( ;; ) {  // for every char in line
+	*infile >> ch;					// read a character
+	if ( infile->fail() ) break A;			// Check fail to exit first loop 
+	*outfile << ch;					// write a character
+	if ( ch == '\n' ) break B;                      // Check for newline char
+      } //for
+    } // for
+    */
+    
+
+
+    //* line by line
+    string line;
+    for (;;){ 
+      Grammar grammar;
+      getline(*infile, line);
+      if ( infile->fail() ) break;
+      *outfile << "\"" << line <<  "\" : ";
+      
+      if (line == ""){
+	*outfile << "Warning! Blank line.";
+      }
+      
+      else {
+	
+	*outfile << "\"";
+      
+	for (unsigned int i = 0; i < line.length(); i+=1){
+	  
+	  *outfile << line[i];
+	  
+	  Grammar::Status status = grammar.next( line[i] );
+
+	  if ( status  == Grammar::CONT ){
+	  }
+
+	  else {
+	    *outfile << "\"";
+	  
+	    if ( status == Grammar::MATCH ){
+	      *outfile << " yes";
+	    }
+	    else if ( status == Grammar::ERROR ){
+	      *outfile << " no";
+	    }
+	    
+	    if ( i != line.length()-1 ){
+	      *outfile << " -- extraneous characters ";
+	      *outfile << "\"" << line.substr(i+1) << "\"";
+	    }
+	    break;
+	  }
+	  
+	}
+		
+      }
+      *outfile << endl;
+    }
+
+    if ( infile != &cin ) delete infile;		// close file, do not delete cin!
+    if ( outfile != &cout ) delete outfile;		// close file, do not delete cout!
+
+} // uMain::main
+
+
+
--- a1/q3/q3grammar.h
+++ a1/q3/q3grammar.h
@@ -4,19 +4,67 @@
 #include <uC++.h>
 
 _Coroutine Grammar {
-  public:
-    enum Status { CONT, MATCH, ERROR };			// send more characters, in language, invalid
-  private:
-    Status status;					// current status of match
-    char ch;						// character passed by cocaller
+ public:
+  enum Status { CONT, MATCH, ERROR };			// send more characters, in language, invalid
+ private:
+  Status status;					// current status of match
+  char ch;						// character passed by cocaller
 
-    void main();
-  public:
-    Status next( char c ) {
-	ch = c;						// communication in
-	resume();					// activate
-	return status;					// communication out
-    } // Grammar::next
+  void main(){
+
+    // First a
+    if ( ch == 'a'){
+      status = CONT;
+    }
+    else{
+      status = ERROR;
+    }
+    suspend();
+
+
+    // At least 1 b
+    if ( ch == 'b'){
+      status = CONT;
+    }
+    else{
+      status = ERROR;
+    }
+    suspend();
+
+
+    // 0 or more b's
+    while ( ch == 'b'){
+      status = CONT;
+      suspend();
+    }
+    
+
+    // 0 or more c's
+    while ( ch == 'c'){
+      status = CONT;
+      suspend();
+    }
+
+
+    // Last d
+    if ( ch == 'd'){
+      status = MATCH;
+    }
+    else{
+      status = ERROR;
+    }
+    suspend();
+
+
+  }
+
+
+ public:
+  Status next( char c ) {
+    ch = c;						// communication in
+    resume();					// activate
+    return status;					// communication out
+  } // Grammar::next
 }; // Grammar
 
 #endif // __GRAMMAR_H__
